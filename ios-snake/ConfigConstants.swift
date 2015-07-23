d488db0aa7834deb605c5cdbd5906e9f1b868b87
Joey <j.lai@live.ca>
2015-07-22 22:25:52 -0600

    Update grid size

--- ios-snake/ConfigConstants.swift
+++ ios-snake/ConfigConstants.swift
@@ -3,6 +3,6 @@ struct ConfigConstants {
     static let DEBUG = true
     
     static let START_SNAKE_LENGTH = 3
-    static let GAME_GRID_ROWS = 35
-    static let GAME_GRID_COLS = 25
+    static let GAME_GRID_ROWS = 45
+    static let GAME_GRID_COLS = 30
 }
