9e8ac82253178e2770f021bc5caa4b7e9a23f82c
Joey <j.lai@live.ca>
2015-08-29 19:03:42 -0600

    Added temporary back button on multiplayer view

--- ios-snake/MultiplayerGameController.swift
+++ ios-snake/MultiplayerGameController.swift
@@ -1,5 +1,8 @@
 import UIKit
 
 class MultiplayerGameController: UIViewController {
-    
+ 
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
+    }
 }
